b38f54c1f842bf8ddb6302589e3c521656b8560f
Terkiey <terkiey@gmail.com>
2025-12-02 12:56:38 +0000

    C# - Part Two Completed, took me ages to realise my for loop for factors was not allowing breaking the numbers into single digits.

--- C#/Day2/Program.cs
+++ C#/Day2/Program.cs
@@ -6,30 +6,36 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        
         string input = File.ReadAllLines("day2input.txt")[0];
 
         IIdParser _idParser = new IdParser();
         List<string[]> ranges = _idParser.ParseIdRanges(input);
 
         double PartOneSum = 0;
+        double PartTwoSum = 0;
         IIdValidator _idValidator = new IdValidator();
         foreach (string[] range in ranges)
         {
             double rangeStart = double.Parse(range[0]);
             double rangeEnd = double.Parse(range[1]);
 
-            for(double id = rangeStart; id < rangeEnd; id++)
+            for(double id = rangeStart; id <= rangeEnd; id++)
             {
                 string idString = id.ToString();
-                if (!_idValidator.ValidateId(idString))
+                if (!_idValidator.ValidateIdPartOne(idString))
                 {
                     PartOneSum += id;
+                }
+
+                if (!_idValidator.ValidateIdPartTwo(idString))
+                {
+                    PartTwoSum += id;
                 }
             }
         }
 
-        Console.WriteLine(PartOneSum);
+        Console.WriteLine($"Part One Answer : {PartOneSum}");
+        Console.WriteLine($"Part Two Answer : {PartTwoSum}");
         await Task.Delay(5000);
     }
 }
