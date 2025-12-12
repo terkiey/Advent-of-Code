94cec0a764bc71a430de477b76eb15d030ae3461
Terkiey <terkiey@gmail.com>
2025-12-12 19:59:22 +0000

    Solved day 12. AoC 2025 is done!

--- C#/AoC/Days/12/Day12.cs
+++ C#/AoC/Days/12/Day12.cs
@@ -4,6 +4,20 @@ internal class Day12: Day
 {
     protected override void RunLogic(string[] lines)
     {
-        
+        IPresentAndTreeProblemParser parser = new PresentAndTreeProblemParser(lines);
+        PresentShape[] presentShapes = parser.GetPresents();
+        TreeProblem[] treeProblems = parser.GetProblems();
+
+        ITreeProblemSolver solver = new TreeProblemSolver(presentShapes, treeProblems);
+        int problemCount = treeProblems.Length;
+        int AnswerOneNum = 0;
+        for (int i = 0; i < problemCount; i++)
+        {
+            if (solver.Solvable(i))
+            {
+                AnswerOneNum++;
+            }
+        }
+        AnswerOne = AnswerOneNum.ToString();
     }
 }
---
+++ C#/AoC/Days/12/PresentAndTreeProblemParser/objects/PresentShape.cs
@@ -0,0 +1,5 @@
+﻿namespace AoC.Days;
+
+internal record PresentShape(bool[,] presentArea)
+{
+}
