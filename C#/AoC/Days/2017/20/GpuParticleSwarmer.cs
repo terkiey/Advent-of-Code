65ff28be744dd7df9823d587406c4a967e9f3184
Terkiey <terkiey@gmail.com>
2026-01-12 12:49:53 +0000

    Y2017Day20 solved.
    Implemented roughly the maths I had set out, and it worked. Tweaked the number of ticks to simulate to be low to reduce un-needed work.
    
    Definitely could improve it by using integer maths rather than floating point on the quadratic formula checker, but it works properly as it is.

--- C#/AoC/Days/2017/20/GpuParticleSwarmer.cs
+++ C#/AoC/Days/2017/20/GpuParticleSwarmer.cs
@@ -1,8 +1,15 @@
-﻿namespace AoC.Days;
+﻿using System.Numerics;
+
+namespace AoC.Days;
 
 internal class GpuParticleSwarmer
 {
     private List<GpuParticle> Particles { get; } = [];
+
+    // Simulation Data
+    private const int TickCount = 5; // Simulation ticks per infinite particle check.
+    private List<GpuParticle> LiveParticles { get; set; } = [];
+    private int InfiniteParticleCount { get; set; } = 0;
 
     public void ReadParticleData(string[] particleDatas)
     {
@@ -133,16 +140,186 @@ internal class GpuParticleSwarmer
          * But how do we do it for a pair of particles?...
          * 
          * The formula for a particle's position p on an axis given x ticks have passed is (v and a are that axis' velocity and acceleration respectively):
-         * p + (v + (v+a) + (v+2a) + (v+3a)+ ... (v+xa)) =
+         * p + (v+a) + (v+2a) + (v+3a) + ... (v+xa)) =
          * p + xv + (a + 2a + ... + xa) =
          * p + xv + a(1 + 2 + 3 + ... + x) =
          * p + xv + a((x+1)x/2)
          * 
          * So we can solve for two different particles, if these formulas are equal to eachother...
-         * p1 - p2 + xv1 - xv2 + a1((x+1)x/2) - a2((x+1)x/2) = 0
+         * p1 - p2 + (v1 - v2)x + a1((x+1)x/2) - a2((x+1)x/2) = 0
+         * (p1 - p2) + (v1 - v2)x + ((a1 - a2)/2)x  + ((a1 - a2)/2)x^2
          * This can be solved with the quadratic formula (as its a quadratic equation), and if any positive roots exist that means a collision will occur, atleast for the position at time x on this axis.
+         * a = (a1 - a2) / 2
+         * b = (v1 - v2) + ((a1 - a2) / 2) = (v1 - v2) + a
+         * c = (p1 - p2)
          * 
-         * So we invert it: if for any axis, there are no postive roots, then the particles will surely never collide.
+         * So we invert it: if for any axis, there are no positive roots, then the particles will surely never collide.
          */
+
+        // Step zero: Take all the particles and put a reference to each into the LiveParticles list.
+        LiveParticles.AddRange(Particles);
+
+        // First, determine if there are any collisions at start state, destroy those particles.
+        HashSet<GpuParticle> collisions = DetermineCollisions(LiveParticles);
+        LiveParticles = [.. LiveParticles.Except(collisions)];
+
+        // Secondly, begin the tick-simulation and infinite particle checks until there are no more 'live' particles.
+        while (LiveParticles.Count > 1)
+        {
+            // Run a suitable amount of ticks.
+            for (int tick = 0; tick < TickCount; tick++)
+            {
+                RunOneTick(LiveParticles);
+                // Check for collisions after each tick, and destroy those particles.
+                collisions = DetermineCollisions(LiveParticles);
+                LiveParticles.RemoveAll(p => collisions.Contains(p));
+
+                // If there is only 1 or 0 live particles left, we can stop simulating (no collision is possible)
+                if (LiveParticles.Count <= 1)
+                {
+                    break;
+                }
+            }
+
+            // If there is 1 or 0 live particles left, break the loop early.
+            if (LiveParticles.Count <= 1)
+            {
+                break;
+            }
+
+            // Use quadratic formula to compare particles and determine if any particles cannot ever collide with all others.
+            HashSet<GpuParticle> collidables = [];
+            HashSet<GpuParticle> infinites = [];
+
+            // The method here is to compare every single pair of particles, and after a particle has been fully compared, we check if it is in the (potentially) collidables set, and if not, then its infinite.
+            for (int indexOne = 0; indexOne < LiveParticles.Count - 1; indexOne++)
+            {
+                GpuParticle particleOne = LiveParticles[indexOne];
+                for (int indexTwo = indexOne + 1; indexTwo < LiveParticles.Count; indexTwo++)
+                {
+                    GpuParticle particleTwo = LiveParticles[indexTwo];
+
+                    if (!SurelyWontCollide(particleOne, particleTwo))
+                    {
+                        collidables.Add(particleOne);
+                        collidables.Add(particleTwo);
+                    }
+                }
+
+                if (!collidables.Contains(particleOne))
+                {
+                    infinites.Add(particleOne);
+                }
+            }
+
+            // For any infinite particle, we remove it from the live particle list, because we dont need to simulate it anymore.
+            LiveParticles.RemoveAll(p => infinites.Contains(p));
+            InfiniteParticleCount += infinites.Count;
+        }
+
+        InfiniteParticleCount += LiveParticles.Count;
+        LiveParticles.Clear();
+        return InfiniteParticleCount.ToString();
+    }
+
+    private HashSet<GpuParticle> DetermineCollisions(List<GpuParticle> particleList)
+    {
+        // Compare every pair of particles, and if their positions are equal, add both to the collided particles set.
+        HashSet<GpuParticle> collidedParticles = [];
+        for (int indexOne = 0; indexOne < particleList.Count - 1; indexOne++)
+        {
+            GpuParticle particleOne = particleList[indexOne];
+            for (int indexTwo = indexOne + 1; indexTwo < particleList.Count; indexTwo++)
+            {
+                GpuParticle particleTwo = particleList[indexTwo];
+                if (particleOne.Position.SequenceEqual(particleTwo.Position))
+                {
+                    collidedParticles.Add(particleOne);
+                    collidedParticles.Add(particleTwo);
+                }
+            }
+        }
+        return collidedParticles;
+    }
+
+    private void RunOneTick(List<GpuParticle> particles)
+    {
+        foreach(GpuParticle particle in particles)
+        {
+            for (int axis = 0; axis < 3; axis++)
+            {
+                particle.Velocity[axis] += particle.Acceleration[axis];
+                particle.Position[axis] += particle.Velocity[axis];
+            }
+        }
+    }
+
+    // I could make this better by turning this check into pure integer maths (rather than floating point) but its accurate for this use-case right now.
+    private bool SurelyWontCollide(GpuParticle particleOne, GpuParticle particleTwo)
+    {
+        // Two particles will never collide if the quadratic equation for their position on any of the 3 axes being equal has no non-negative (cant be in the past), integer (we run on ticks, not continuous time) roots.
+        // root = (-b +- sqrt((b^2) - 4ac)) / 2a
+
+        for (int axis = 0; axis < 3; axis++)
+        {
+            double a1 = (double)particleOne.Acceleration[axis];
+            double a2 = (double)particleTwo.Acceleration[axis];
+            double v1 = (double)particleOne.Velocity[axis];
+            double v2 = (double)particleTwo.Velocity[axis];
+            double p1 = (double)particleOne.Position[axis];
+            double p2 = (double)particleTwo.Position[axis];
+
+            double a = (a1 - a2) / 2f;
+
+            if (a == 0)
+            {
+                return SurelyWontLinearCollide1D(p1, v1, p2, v2);
+            }
+
+            double b = (v1 - v2) + a;
+            double c = (p1 - p2);
+
+            double discriminant = (b * b) - (4 * a * c);
+
+            if (discriminant < 0)
+            {
+                return true;
+            }
+
+            double rootOne = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            if (rootOne >= 0 && Math.Abs(rootOne - Math.Round(rootOne)) < 1e-9)
+            {
+                continue;
+            }
+
+            double rootTwo = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            if (rootTwo >= 0 && Math.Abs(rootTwo - Math.Round(rootTwo)) < 1e-9)
+            {
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private bool SurelyWontLinearCollide1D(double p1, double v1, double p2, double v2)
+    {
+        // Particles get checked here if their accelerations are equal, then we just need to check if they could collide linearly.
+        // That is where p1 + xv1 = p2 + xv2 has a non-negative integer root.
+        // (p1 - p2) + (xv1 - xv2) = 0
+        // (p2 - p1) / (v1 - v2) = x
+        
+        // We know their positions are not equal, because that gets checked at each tick simulation, so if velocities are equal, then they wont collide.
+        if (v1 - v2 == 0)
+        {
+            return true;
+        }
+
+        double root = (p2 - p1) / (v1 - v2);
+        if (root < 0 || Math.Abs(root - Math.Round(root)) >= 1e-9)
+        {
+            return true;
+        }
+        return false;
     }
 }
